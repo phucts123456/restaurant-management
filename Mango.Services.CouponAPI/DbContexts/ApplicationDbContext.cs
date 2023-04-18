fc1356eab090736dc122d5abeea24acf4a2391fc
phucts <hooquangphuc.t4@gmail.com>
2023-04-18 10:32:24 +0700

    add coupon API

---
+++ Mango.Services.CouponAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Mango.Services.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.CouponAPI.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Coupon> Coupons { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                CouponCode = "10OFF",
+                CouponId = 1,
+                DiscountAmount = 10,
+            });
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                CouponCode = "20OFF",
+                CouponId = 2,
+                DiscountAmount = 20,
+            });
+        }
+    }
+}
---
+++ Mango.Services.CouponAPI/Repository/CouponRepository.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Mango.Services.CouponAPI.DbContexts;
+using Mango.Services.CouponAPI.Models;
+using Mango.Services.CouponAPI.Models.Dto;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.CouponAPI.Repository
+{
+    public class CouponRepository : ICouponRepository
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public CouponRepository(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<CouponDto> GetCouponByCode(string couponCode)
+        {
+            var couponFromDb = await _context.Coupons.FirstOrDefaultAsync(u => u.CouponCode == couponCode);
+            return _mapper.Map<CouponDto>(couponFromDb);
+        }
+    }
+}
